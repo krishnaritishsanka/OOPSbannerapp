a4828deab5af073f5a2c8aaadc9224c6a447d688
ns1452-lab <ns1452@srmist.edu.in>
2026-02-23 13:13:13 +0530

    UC4

---
+++ Oopsbannerappuc4.java
@@ -0,0 +1,23 @@
+
+
+public class OopsBannerappuc4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            String.join("", " *****  ", " *****  ", " ***** " , " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*    *"),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*     * ", "******  ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*       ", "     * "),
+            String.join("", "*     * ", "*     * ", "*       ", "*    * "),
+            String.join("", " *****  ", " *****  ", "*.      ", " ***** ")
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
